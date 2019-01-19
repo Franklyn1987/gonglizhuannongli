bd1e9961f25a2d3b77cd5ebeec38bef82dc078df
Franklyn <baiyuexingchen@gmail.com>
2019-01-20 07:32:00 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='lunar',
+    version='2.0',
+    description='公历转农历',
+    author='崔立波',
+    author_email='cuilibo@gmail.com',
+    url='https://github.com/Franklyn1987',
+    py_modules=['lunar'],
+)
